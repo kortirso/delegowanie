1d1653e2f5779ea886a20ad6381e00c7e3320a67
kortirso <kortirso@gmail.com>
2017-10-18 14:44:54 +0300

    add index page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+    def index; end
+
+    def contacts; end
+
+    def uslugi; end
+
+    def about; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
 Rails.application.routes.draw do
+    resources :pages, only: %i[index] do
+        get :contacts, on: :collection
+        get :uslugi, on: :collection
+        get :about, on: :collection
+    end
+
+    root to: 'pages#index'
 end
